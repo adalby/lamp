e8b6056931608b5461b19fda95ec9d259de79dcd
Andrew Dalby <adalby@yeomania.org>
2018-10-29 20:21:38 +0000

    Added package script to install, update LAMP Stack packages

---
+++ 1.pkg.sh
@@ -0,0 +1,33 @@
+# 2017 Jun 01 RAD Start with a fresh, fully updated system
+sudo yum update -y
+
+# 2017 Jun 01 RAD Add Extra Package repo, utils that should have been installed by default
+sudo yum install epel-release
+sudo yum install mailx wget -y
+
+# 2017 Jun 01 RAD Install emacs & git for development, deployment
+sudo yum install emacs git -y
+
+# 2017 Jun 01 RAD Second tier of a LAMP stack is the Database Server
+sudo yum install mariadb mariadb-server -y
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+
+# 2017 Jun 01 RAD Next up is the Web Server
+sudo yum install httpd php php-mysql php-gd -y
+sudo systemctl enable httpd
+sudo systemctl start httpd
+
+# 2018 Oct 29 RAD Download latest copy of WordPress
+wget https://wordpress.org/latest.zip
+
+# 2017 Aug 30 RAD Get certbot for https
+# Enable EPEL Optional Channels per https://certbot.eff.org/all-instructions/#centos-rhel-7-apache
+sudo yum -y install yum-utils
+sudo yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
+#sudo yum install certbot python-certbot-apache -y
+sudo yum install certbot-apache -y
+
+
+# 2017 Jun 07 RAD Finish up by making sure everything is up to date
+sudo yum update -y
